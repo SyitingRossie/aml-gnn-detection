f8ce4be9c7da98fe1401374cd827a4d0cac2950c
SyitingRossie <syiting403@gmail.com>
2026-08-24 10:47:31 +0800

    Optimize ImprovedGAT model and remove original code
    Refactor ImprovedGAT model with updated imports and structure.

--- optimized_code/Model.py
+++ optimized_code/Model.py
@@ -1,4 +1,10 @@
+# 优化后模型
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.transforms as T
 from torch_geometric.nn import GATv2Conv
+
 class ImprovedGAT(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, heads=8, edge_dim=None):
         super().__init__()
@@ -45,23 +51,27 @@ class ImprovedGAT(torch.nn.Module):
 
 
 # 8.模型构建[ORIGINAL] （参考）
-# class GAT(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels, heads, edge_dim):
-#         super().__init__()
-#         self.conv1 = GATConv(in_channels=in_channels, out_channels=hidden_channels, heads=heads, dropout=0.3,
-#                              add_self_loops=True,
-#                              edge_dim=edge_dim)  # 卷积加入自生原始特征！！！#dropout从0.6改成0.3 ,原作者没有加入边特征！！！！！！！！！！！！1
-#         self.conv2 = GATConv(hidden_channels * heads, int(hidden_channels / 4), heads=1,
-#                              dropout=0.3,
-#                              edge_dim=edge_dim)  # concat=False，这句没加，因为concat只有在heads>1时才有区别。concat=True是默认的，横向拼接所有头的特征，如果是False，则是取平均，不拼接。
-#         self.lin = Linear(int(hidden_channels / 4), out_channels)  # 从4到1
-#         # self.sigmoid = nn.Sigmoid()
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torch_geometric.transforms as T
+# from torch_geometric.nn import GATConv, Linear
 
-#     def forward(self, x, edge_index, edge_attr):  # edge_index是二维张量，第一行是from的账户index，第二行是to的账户index。   输入x：是节点特征
-#         x = F.dropout(x, p=0.3, training=self.training)
+# class GAT(torch.nn.Module):
+#     def __init__(self, in_channels, hidden_channels, out_channels, heads):
+#         super().__init__()
+#         self.conv1 = GATConv(in_channels, hidden_channels, heads, dropout=0.6)
+#         self.conv2 = GATConv(hidden_channels * heads, int(hidden_channels/4), heads=1, concat=False, dropout=0.6)
+#         self.lin = Linear(int(hidden_channels/4), out_channels)
+#         self.sigmoid = nn.Sigmoid()
+
+#     def forward(self, x, edge_index, edge_attr):
+#         x = F.dropout(x, p=0.6, training=self.training)
 #         x = F.elu(self.conv1(x, edge_index, edge_attr))
-#         x = F.dropout(x, p=0.3, training=self.training)
+#         x = F.dropout(x, p=0.6, training=self.training)
 #         x = F.elu(self.conv2(x, edge_index, edge_attr))
-#         logits = self.lin(x)
-#         return logits.squeeze(dim=1)
+#         x = self.lin(x)
+#         x = self.sigmoid(x)
+        
+#         return x
 
